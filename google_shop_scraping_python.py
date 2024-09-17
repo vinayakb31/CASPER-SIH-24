f1179337432267a09ed83866065c21e36c6c1dd2
Vinayak <93078680+vinayakb31@users.noreply.github.com>
2024-09-17 19:14:00 +0530

    Update google_shop_scraping_python.py
    Changelog:
    - Fixed sorting
    - Added more options for sorting
    - Removed the "#" column (sl. no.)

--- google_shop_scraping_python.py
+++ google_shop_scraping_python.py
@@ -6,10 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 
 
-def SearchAndResponse(search_str):
+search_term = []
+search_category =[]
+
+def SearchAndResponse(search_term):
     global soup
 
-    search_url = f"https://www.google.com/search?tbm=shop&q={search_str.replace(' ', '+')}"
+    search_url = f"https://www.google.com/search?tbm=shop&q={' '.join(search_term[-1]).replace(' ', '+')}"
     HEADERS = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0', 'Accepted-Language':'en-US, en;q=0.5'})
 
     response = requests.get(search_url, headers=HEADERS)
@@ -22,21 +25,28 @@ def GoogleShopScraping(soup):
     global products_list
     products_list = []
     
-    titles = soup.find_all('h3', {'class':'tAxDx'})
-    prices = soup.find_all('span', {'class':'a8Pemb OFFNJ'})
-    source_name = soup.find_all('div', {'class':'aULzUe IuHnof'})
-    jumbled_links = soup.find_all('div', {'class':'mnIHsc'})
-    source_link = []
+    span_page = soup.find_all('div', {'class':'i0X6df'})
 
-    for i in jumbled_links:
-        jumbled_links_content = BeautifulSoup(str(i), 'html.parser')
-        href_tag = jumbled_links_content.find('a', {'class': 'shntl'})
-        source_link.append(href_tag.get('href'))
-    source_link = ["https://www.google.com"+i for i in source_link]
+    sl_counter = 1
+    for i in range(len(span_page)):
+        curr_prod = span_page[i]
+        if curr_prod.find('div', {'class':'qSSQfd uqAnbd'}) and curr_prod.find('h3', {'class':'tAxDx'}):
+            title = curr_prod.find('h3', {'class':'tAxDx'})
+            price = curr_prod.find('span', {'class':'a8Pemb OFFNJ'})
+            rating = curr_prod.find('div', {'class':'qSSQfd uqAnbd'}).get('aria-label')
+            source_name = curr_prod.find('div', {'class':'aULzUe IuHnof'})
+            jumbled_link = curr_prod.find_all('div', {'class':'mnIHsc'})
 
-    for i in range(len(titles)):
-        products_list.append([i+1, titles[i].text.strip(), prices[i].text.strip(), source_name[i].text.strip(), source_link[i]])
-    
+            for i in jumbled_link:
+                jumbled_link_content = BeautifulSoup(str(i), 'html.parser')
+                href_tag = jumbled_link_content.find('a', {'class': 'shntl'})
+                source_link = "https://www.google.com" + href_tag.get('href')
+
+            products_list.append([sl_counter, title.text.strip(), float(price.text.strip().replace('₹','').replace(',','').replace(' + tax','')),
+                                  rating, source_name.text.strip(), source_link])
+
+            sl_counter += 1
+
     return products_list
 
 
@@ -47,6 +57,8 @@ def jsonInfoDump(products_list):
 
 def Webpage():
     global app
+    global search_term
+    global search_category
 
     app = Flask(__name__)
     
@@ -129,7 +141,6 @@ def Webpage():
     @app.route('/search', methods=["GET", "POST"])
     def search():
         if request.method == "POST":
-            # Get the form data
             item_name = request.form.get("item_name")
             item_type = request.form.get("item_type")
             make = request.form.get("make")
@@ -151,12 +162,32 @@ def Webpage():
         make = request.args.get('make')
         model = request.args.get('model')
 
-        SearchAndResponse(str(item_name + model + make))
+        if item_name is not None:
+            search_term.append([item_name, model, make])
+            search_category.append(item_type)
+
+        print(search_term[-1])
+        print(search_category[-1])
+
+        SearchAndResponse(search_term)
         GoogleShopScraping(soup)
         jsonInfoDump(products_list)
 
         with open('product_details.json', 'r') as f:
             product_details = json.load(f)
+
+        sort_by = request.args.get('sort_by', 'price')
+        sort_direction = request.args.get('sort_direction', 'asc')
+        reverse = False
+
+        if sort_by == 'price':
+            reverse = (sort_direction == 'desc')
+            product_details.sort(key=lambda x: x[2], reverse=reverse)
+        elif sort_by == 'ratings':
+            product_details.sort(key=lambda x: x[3], reverse=reverse)
+
+        for product in product_details:
+            product[2] = f"₹{product[2]:,.2f}"
 
         page = request.args.get('page', 1, type=int)
         per_page = 20
@@ -176,7 +207,7 @@ def Webpage():
 
 
         return render_template("results.html", product_details=current_products,page=page, total_pages=total_pages,
-                               item_name=item_name, item_type=item_type, make=make, model=model)
+                               item_name=search_term[-1][0], item_type=search_category[-1], make=search_term[-1][2], model=search_term[-1][1], sort_by=sort_by, sort_direction=sort_direction)
 
 if __name__ == "__main__":
     app = Flask(__name__)
